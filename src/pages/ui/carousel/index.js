af8352212d16aede3941523c4621a526e73477bd
zhk_8030 <17362113082@163.com>
2019-09-27 15:14:32 +0800

    轮播图

--- src/pages/ui/carousel/index.js
+++ src/pages/ui/carousel/index.js
@@ -1,6 +1,10 @@
 import React,{ Component } from "react";
+import { Card, Carousel } from 'antd'
+
+import '../ui.less'
 import './index.less';
-class Carousel extends Component {
+
+class Carousels extends Component {
   constructor(props){
     super(props)
     this.state={
@@ -10,13 +14,46 @@ class Carousel extends Component {
   componentWillMount(){
     
   }
+  onChange=(a, b, c)=> {
+    console.log(a, b, c);
+  }
   
   render (){
+    const imgList = [
+      '1.png','2.png','3.png','4.png'
+    ]
     return(
-      <div className="Carousel">
-        this is Carousel
+      <div className="Carousels">
+        <Card title="基础轮播" className="Card_warp">
+          <Carousel autoplay afterChange={this.onChange}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+          </Carousel>
+        </Card>
+
+        <Card title="图片轮播" className="Card_warp">
+          <Carousel autoplay afterChange={this.onChange}>
+            {
+              imgList.map((item)=>
+                <div>
+                  <img src={'/gallery/'+item} alt='轮播图' style={{width:'100%',height:'100%'}}/>
+                </div>
+              )
+            }
+          </Carousel>
+        </Card>
       </div>
     )
   }
 }
-export default Carousel;
+export default Carousels;
--- src/pages/ui/gallery/index.js
+++ src/pages/ui/gallery/index.js
@@ -1,21 +1,81 @@
 import React,{ Component } from "react";
-import { Card, Row, Col } from 'antd'
+import { Card, Row, Col, Modal } from 'antd'
 import './index.less';
+const { Meta } = Card;
 class Gallery extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      
-    }
-  }
-  componentWillMount(){
-    
-  }
+
+  state = { visible: false };
   
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+  openGallery=(data)=>{
+    this.setState({
+      visible:true,
+      ModalImg:data
+    })
+  }
   render (){
+    const imgs= [
+      ['1.png','2.png','3.png','4.png','5.png',],
+      ['6.png','7.png','8.png','9.png','10.png',],
+      ['11.png','12.png','13.png','14.png','15.png',],
+      ['16.png','17.png','18.png','19.png','20.png',],
+      ['21.png','22.png','23.png','24.png','25.png',],
+    ]
+    const imgList = imgs.map((list)=>list.map((item,key)=>
+      <Card
+        cover={<img alt='' src={'/gallery/'+item}  onClick={()=>this.openGallery('/gallery/'+item)} />}
+        style={{marginBottom:10}}
+        key={key}
+        
+      >
+        <Meta title="Europe Street beat" description="www.instagram.com" />  
+      </Card>
+    ))
     return(
       <div className="Gallery">
-        this is Gallery
+        <Row gutter={10}> 
+          <Col span={4}>
+            { imgList[0] }
+          </Col>
+          <Col span={4}>
+            { imgList[1] }
+          </Col>
+          <Col span={4}>
+            { imgList[2] }
+          </Col>
+          <Col span={4}>
+            { imgList[3] }
+          </Col>
+          <Col span={4}>
+            { imgList[4] }
+          </Col>
+          <Col span={4}>
+            { imgList[0] }
+          </Col>
+        </Row>
+        <Modal
+          width={300}
+          height={500}
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={null}
+        >
+          <p><img alt='Modal_img' src={this.state.ModalImg} style={{width:'100%',height:'100%'}} /></p>
+        </Modal>
       </div>
     )
   }
